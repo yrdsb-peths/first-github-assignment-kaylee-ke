d82e70e8bab324c621b727f89f6adfcf601eb4c1
kaylee-ke <131057050+kaylee-ke@users.noreply.github.com>
2023-04-19 08:33:19 -0400

    return a rectangle

---
+++ Rectangle.java
@@ -0,0 +1,16 @@
+public class Rectangle
+{
+    private int width;
+    private int height;
+    
+    public Rectangle(int rectWidth, int rectHeight)
+    {
+        width = rectWidth;
+        height = rectHeight;
+    }
+    
+    public String toString()
+    {
+        return "Rectangle with width: " + width + " and height: " + height;
+    }
+}    
