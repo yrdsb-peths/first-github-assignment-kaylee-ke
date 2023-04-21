c3e1891c2761f6f8ee76210789292333b2e26fa7
kaylee-ke <131057050+kaylee-ke@users.noreply.github.com>
2023-04-21 08:55:50 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,12 +3,14 @@ public class Student
     private String firstName;
     private String lastName;
     private int gradeLevel;
+    private int gpa;
     
-    public Student(String fName, String lName, int grade)
+    public Student(String fName, String lName, int grade, int theGPA)
     {
         firstName = fName;
         lastName = lName;
         gradeLevel = grade;
+        gpa = theGPA;
     }
     
     public String toString()
